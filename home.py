1b15ae9232453298022fc38f6e086d29e8e4d2cf
LCJ1324 <chn4567@gmail.com>
2025-07-13 23:26:22 +0900

    Add files via upload

--- home.py
+++ home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 pages = [
-    st.Page("main.py", title="메인페이지", icon="🟠", default=True),
+    st.Page("main.py", title="사업소 소개", icon="🟠", default=True),
     st.Page("data.py", title="사업소별 데이터", icon="🟢"),
     st.Page('machinelearning.py', title = 'NOX 배출량 예측', icon="🔵"),
     # st.Page("hyper.py", title="머신러닝", icon="🤖")
